74966626868fb6cfc0b7bb8f6c2c5775dc5b164a
Austin Jeremy Dunn <austin.jeremy.dunn@gmail.com>
2025-07-11 16:32:19 -0400

    Consolidating njk into a single file that can be translated according to _data/languages.js

---
+++ src/_data/languages.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        lang: 'en',
+        permalink: '/',
+        title: 'Sarah Foulkes'
+    },
+    {
+        lang: 'fr',
+        permalink: '/fr/',
+        title: 'Sarah Foulkes'
+    }
+];
